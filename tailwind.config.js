a8194c98745fcbfeb650ab1bb1521ca644c80dd5
hagansamuel6 <hagansamuel6@gmail.com>
2021-08-16 14:53:23 +0000

    ashanti unique market

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
